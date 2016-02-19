513bc0d6a0cc04673fe7447aefca87d76ae81eb2
James <jsmit106@asu.edu>
2016-02-19 10:30:58 -0700

    modified AccountServerFactory to use static methods rather than instantiating a singleton with no state

--- given/src/banking/primitive/core/AccountServerFactory.java
+++ given/src/banking/primitive/core/AccountServerFactory.java
@@ -1,23 +1,9 @@
 package banking.primitive.core;
 
 
-public class AccountServerFactory {
-
-	protected static AccountServerFactory singleton = null;
-
-	protected AccountServerFactory() {
-
-	}
-
-	public static AccountServerFactory getMe() {
-		if (singleton == null) {
-			singleton = new AccountServerFactory();
-		}
-
-		return singleton;
-	}
-
-	public AccountServer lookup() {
+public interface AccountServerFactory {
+	
+	public static AccountServer lookup() {
 		return new ServerSolution();
 	}
 }
